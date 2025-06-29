80cd45ea24224f32c4663aaa104f1dc99f3abef2
Swetabh Mukherjee <swetabh.libra@gmail.com>
2025-06-30 01:31:31 +0530

    Add support for newline as additional delimiter

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,9 +1,9 @@
 function add(input) {
+  const delimiter = /,|\n/;
+  const parts = input.split(delimiter);
   if (input === '') return 0;
 
-  const parts = input.split(',');
   if (parts.length === 1) return parseInt(parts[0], 10);
-
   return parts.reduce((sum, n) => sum + parseInt(n, 10), 0);
 }
 
--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -15,3 +15,7 @@ test('returns sum of two numbers', () => {
 test('returns sum of multiple numbers', () => {
   expect(add('1,2,3,4')).toBe(10);
 });
+
+test('supports new lines between numbers', () => {
+  expect(add('1\n2,3')).toBe(6);
+});
