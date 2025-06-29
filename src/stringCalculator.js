cd881203714cf047db1ee4e1968b2bbaa0a1d07b
Swetabh Mukherjee <swetabh.libra@gmail.com>
2025-06-30 00:51:26 +0530

    Return single number if only one number is passed

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,5 +1,6 @@
 function add(input) {
   if (input === '') return 0;
+  return parseInt(input, 10);
 }
 
 module.exports = { add };
--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -3,3 +3,7 @@ const { add } = require('../src/stringCalculator')
 test('returns 0 for empty string', () => {
   expect(add('')).toBe(0);
 });
+
+test('returns number for single number', () => {
+  expect(add('4')).toBe(4);
+});
